8d049d05d3a851cf2f046af18601bf16476b5421
jirshaid <jirshaid@luc.edu>
2025-09-10 17:16:24 -0500

    Update HW1.java

--- HW1.java
+++ HW1.java
@@ -1,6 +1,6 @@
 
 /*
- * *** PLACE YOUR NAME / SECTION  HERE ***
+ * *** Jenine Irshaid / 002 ***
  *
  * Homework # 1 (Programming Assignment). This Java class defines some basic
  * manipulation operations on Linked-Lists and Stacks.
